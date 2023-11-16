d297c36c680864e160ab67ab03f09daa43f5cf48
kristiafiliposki <filiposki.kristian1112@gmail.com>
2023-11-16 09:40:24 +0100

    i delete let foto

--- java/main.js
+++ java/main.js
@@ -1,12 +1,12 @@
-const fotosrc="https://wildcard.codestuff.io/dog/250/250";
 
 const { createApp } = Vue;
 
 const opzioni = {
-    data: function (){
+    data(){
         return {
             titoloFoto: `inserisci il nome dell'animale`,
-            foto: fotosrc
+            foto: "https://wildcard.codestuff.io/dog/250/250",
+
 
         }
     }
